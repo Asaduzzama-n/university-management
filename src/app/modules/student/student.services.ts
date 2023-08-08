5ce693a40274895f708954d949d19c73bdd97fc3
Asaduzzaman <masaduzzaman193146@bscse.uiu.ac.bd>
2023-08-08 10:39:03 +0600

    [DONE] upto getAllStudents

--- src/app/modules/student/student.services.ts
+++ src/app/modules/student/student.services.ts
@@ -1,6 +1,11 @@
+import { SortOrder } from 'mongoose';
 import { Request, Response } from 'express';
-import { IStudent } from './student.interfaces';
+import { IStudent, IStudentFilters } from './student.interfaces';
 import { Student } from './student.model';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { PaginationHelpers } from '../../../helpers/paginationHelper';
+import { studentSearchableFields } from './student.constants';
 
 const getSingleStudent = async (id: string): Promise<IStudent | null> => {
   const result = await Student.findOne({ id })
@@ -11,6 +16,64 @@ const getSingleStudent = async (id: string): Promise<IStudent | null> => {
   return result;
 };
 
+const getAllStudent = async (
+  filters: IStudentFilters,
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IStudent[]>> => {
+  const { searchTerm, ...filtersData } = filters;
+  const { page, limit, sortBy, skip, sortOrder } =
+    PaginationHelpers.calculatePagination(paginationOptions);
+
+  const andConditions = [];
+  // Extract searchTerm to implement search query
+  if (searchTerm) {
+    andConditions.push({
+      $or: studentSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+  // Filters needs $and to full fill all the conditions
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([filed, value]) => ({
+        [filed]: value,
+      })),
+    });
+  }
+
+  // Dynamic  Sort needs  field to  do sorting
+  const sortConditions: { [key: string]: SortOrder } = {};
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+  const whereConditions =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const result = await Student.find(whereConditions)
+    .populate('academicSemester')
+    .populate('academicDepartment')
+    .populate('academicFaculty')
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await Student.countDocuments(whereConditions);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const StudentService = {
   getSingleStudent,
+  getAllStudent,
 };
